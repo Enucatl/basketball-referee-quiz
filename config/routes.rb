61acc54b15f624fbc49bc237ef286d076c02520a
Matteo Abis <gmatteo..abis@gmail.com>
2014-08-26 18:31:39 +0200

    random question posted

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
       resources :answers
   end
 
+  get 'question/' => 'questions#random'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
