cfc15a5fb4e8163e0bf0d9e124c327da4f1e4f97
Matteo Abis <gmatteo..abis@gmail.com>
2014-08-26 16:43:19 +0200

    added tags to questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826100459) do
+ActiveRecord::Schema.define(version: 20140826144212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: true do |t|
-    t.text     "text"
-    t.boolean  "is_correct?"
+    t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_correct?"
+    t.text     "text"
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.text     "text"
@@ -30,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140826100459) do
     t.float    "volatility"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "tag"
   end
 
   create_table "users", force: true do |t|
