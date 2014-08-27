c0ba779f552af64020ef2c8a241b2ae10ddf918c
Matteo Abis <gmatteo..abis@gmail.com>
2014-08-27 13:48:04 +0200

    result computed correctly with time info

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,23 @@
 class QuestionsController < ApplicationController
+
+    before_action :authenticate_user!
+
     def random
         @question = Question.order_by_rand.first
     end
 
     def check
         answer = Answer.find(params[:answer_id])
+        question = answer.question
+        puts question.rating
         if answer.is_correct?
             puts "RIGHT!!!"
+            user_result = Result::get_result params[:time].to_f
         else
             puts "WRONG"
+            user_result = 0
         end
+        puts user_result
         redirect_to action: "random"
     end
 end
---
+++ app/models/result.rb
@@ -0,0 +1,24 @@
+module Result
+    #In case of an unsolved problem the result of the user vs. problem
+    #'match' will be 0:1. If a problem is solved, the result is depending on
+    #the elapsed time. The first t0 seconds are for free and the result will
+    #be 1:0. After t0 seconds the result is linearly decreasing with 1/2:1/2
+    #at t1 seconds. After t1 seconds the decrease of the result is
+    #exponential with constant tau
+    @@t0 = 1
+    @@t1 = 3
+    @@delta = @@t1 - @@t0
+    @@m = - 0.5 / @@delta
+    @@tau = 5
+    @@k = - 1 / @@tau
+
+    def self.get_result(time)
+        if time < @@t0
+            return 1
+        elsif time > @@t0 and time < @@t1
+            return @@m * (time - @@t0) + 1
+        else
+            return 0.5 * Math.exp(@@k * (time - @@t1))
+        end
+    end
+end
