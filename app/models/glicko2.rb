1c4daca00020fed56ec85a3b2e16e8e9410dec1d
Matteo Abis <gmatteo..abis@gmail.com>
2014-08-28 10:02:52 +0200

    ratings are updated

--- app/models/glicko2.rb
+++ app/models/glicko2.rb
@@ -20,44 +20,44 @@ module Glicko2
         1 / Math.sqrt(1 + 3 * phi ** 2 / Math::PI ** 2)
     end
 
-    def self.E mu, mu_opp, phi_opp
-        1 / (1 + Math.exp(-self.g(phi_opp) * (mu - mu_opp)))
+    def self.e mu, mu_opp, phi_opp
+        1 / (1 + Math.exp(-g(phi_opp) * (mu - mu_opp)))
     end
 
     def self.v mu, mu_opp, phi_opp
-        e = self.E mu, mu_opp, phi_opp
-        1 / (g(phi_opp) ** 2 * e * (1 - e))
+        e_ = e mu, mu_opp, phi_opp
+        1 / (g(phi_opp) ** 2 * e_ * (1 - e_))
     end
 
     def self.delta result, mu, mu_opp, phi_opp
-        self.v(mu, mu_opp, phi_opp) * g(phi_opp) * (result - self.E(mu, mu_opp, phi_opp))
+        v(mu, mu_opp, phi_opp) * g(phi_opp) * (result - e(mu, mu_opp, phi_opp))
     end
 
-    def self.new_volatility volatility, phi, delta, v
+    def self.new_volatility volatility, phi, delta_, v_
         a = Math.log(volatility ** 2)
-        def self.f x 
+        f = lambda do |x|
             ex = Math.exp x 
-            up1 = ex * (delta ** 2 - phi ** 2 - v - ex)
+            up1 = ex * (delta_ ** 2 - phi ** 2 - v_ - ex)
             up2 = (x - a)
-            down1 = 2 * (phi ** 2 + v + ex) ** 2
+            down1 = 2 * (phi ** 2 + v_ + ex) ** 2
             down2 = @@tau ** 2
             up1 / down1 - up2 / down2
         end
         big_A = a
-        if delta ** 2 > phi ** 2 + v
-            big_B = Math.log(delta ** 2 - phi ** 2 - v)
+        if delta_ ** 2 > phi ** 2 + v_
+            big_B = Math.log(delta_ ** 2 - phi ** 2 - v_)
         else
             k = 1
-            while f(a - k * @@tau) < 0
+            while f.call(a - k * @@tau) < 0
                 k += 1
             end
             big_B = a - k * @@tau
         end
-        fA = f(big_A)
-        fB = f(big_B)
-        while Math.abs(big_B - big_A) > @@epsilon
+        fA = f.call(big_A)
+        fB = f.call(big_B)
+        while (big_B - big_A).abs > @@epsilon
             big_C = big_A + (big_A - big_B) * fA / (fB - fA)
-            fC = f(big_C)
+            fC = f.call(big_C)
             if fC * fB < 0
                 big_A = big_B
                 fA = fB
@@ -70,24 +70,24 @@ module Glicko2
         Math.exp(big_A / 2)
     end
 
-    def self.new_rating mu, phi, result, new_volatility, v, g, e
+    def self.new_rating mu, phi, result, new_volatility, v_, g_, e
         phi_star = Math.sqrt(phi ** 2 + new_volatility ** 2)
-        new_phi = 1 / Math.sqrt(1 / phi_star ** 2 + 1 / v)
-        new_mu = mu + new_phi ** 2 * g * (result - e)
+        new_phi = 1 / Math.sqrt(1 / phi_star ** 2 + 1 / v_)
+        new_mu = mu + new_phi ** 2 * g_ * (result - e)
         return new_mu, new_phi
     end
 
     def self.update_rating rating, deviation, volatility, rating_opp, deviation_opp, result
-        mu, phi = self.scale_old2new rating, deviation
-        mu_opp, phi_opp = self.scale_old2new rating_opp, deviation_opp
-        g = self.g phi
-        e = self.E mu, mu_opp, phi_opp
-        v = self.v mu, mu_opp, phi_opp
-        delta = self.delta result, mu, mu_opp, phi_opp
-        new_volatility = self.new_volatility volatility, phi, delta, v
-        new_mu, new_phi = self.new_rating mu, phi, result, new_volatility, v, g, e
-        new_rating, new_deviation = self.scale_new2old new_mu, new_phi
-        return new_rating, new_deviation, new_volatility
+        mu, phi = scale_old2new rating, deviation
+        mu_opp, phi_opp = scale_old2new rating_opp, deviation_opp
+        g_ = g phi
+        e_ = e mu, mu_opp, phi_opp
+        v_ = v mu, mu_opp, phi_opp
+        delta_ = delta result, mu, mu_opp, phi_opp
+        new_v = new_volatility volatility, phi, delta_, v_
+        new_mu, new_phi = new_rating mu, phi, result, new_v, v_, g_, e_
+        new_rating, new_deviation = scale_new2old new_mu, new_phi
+        return new_rating, new_deviation, new_v
     end
 
 end
