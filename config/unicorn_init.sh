0fbbddbdbfa913e53027309954006df2634a31cc
Matteo Abis <gmatteo..abis@gmail.com>
2014-09-02 19:50:25 +0200

    added rvm string to unicorn

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deploy/basketball-referee-quiz/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; source $(rvm 2.1.2 do rvm env --path); bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deploy
 set -u
 
